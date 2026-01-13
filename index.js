60e3b53b2d1a5d569f0a6087c9b8697b84a3cf82
Dmitriy Zoteev <zdima4444@yandex.ru>
2026-01-13 08:00:44 +0300

    Added functionality

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+	const tasks = localStorage.getItem('tasks');
+	if (tasks) return JSON.parse(tasks);
+	return items;
 }
 
 function createItem(item) {
@@ -22,14 +24,66 @@ function createItem(item) {
   const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
+	
+	textElement.textContent = item;
+
+	deleteButton.addEventListener('click', function() {
+		clone.remove();
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	duplicateButton.addEventListener('click', function() {
+		const itemName = textElement.textContent;
+		const newItem = createItem(itemName);
+		listElement.prepend(newItem);
+		items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	editButton.addEventListener('click', function() {
+		textElement.setAttribute('contenteditable', 'true');
+		textElement.focus();
+	});
+
+	textElement.addEventListener('blur', function() {
+		textElement.setAttribute('contenteditable', 'false');
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	return clone;
 
 }
 
 function getTasksFromDOM() {
+	const itemsNamesElements = document.querySelectorAll('.to-do__item-text');
+	const tasks = [];
+	itemsNamesElements.forEach((item) => {
+		tasks.push(item.textContent);
+	});
 
+	return tasks;
 }
 
 function saveTasks(tasks) {
-
+	localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+
+items = loadTasks();
+
+items.forEach((item) => {
+	const elem = createItem(item);
+	listElement.append(elem);
+});
+
+formElement.addEventListener('submit', function(event) {
+	event.preventDefault();
+	const text = inputElement.value;
+	const elem = createItem(text);
+	listElement.prepend(elem);
+	items = getTasksFromDOM();
+	saveTasks(items);
+	inputElement.value = '';
+})
 
